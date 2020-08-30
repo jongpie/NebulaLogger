ae2163cb50bcbeb06c64627d377deb09852d9611
Jonathan Gillespie <jonathan.c.gillespie@gmail.com>
2020-08-29 18:14:46 -0700

    Summer '20 overhaul
    * Upgraded all metadata to API v49.0 (Summer '20)
    
    * Created new class LogEntryBuilder
    
    * New version of Logger that leverages LogEntryBuilder & log level-specific methods
    
    * Renamed/added fields to distinguish system mode vs log entry-specific context
    
    * Updated UI metadata (layouts, list views, etc)
    
    * Added new component to view log as JSON
    
    * Added new permission set to give view-all access to log objects
    
    * Renamed LightningLogEntry to ComponentLogEntry
    
    * Added 'Related' field prefix in LogEntryHandler
    
    * Renamed LogPurger to LogBatchPurger
    
    * Removed refs to TransactionEntryId__c
    
    * Added alwaysSave() and shouldSave() on LogEntryBuilder
    
    * Embedded new report in log page
    
    * Added setParentLogTransactionId & getParentLogTransactionId

---
+++ nebula-logger/main/logger/aura/logJSONViewer/logJSONViewerHelper.js
@@ -0,0 +1,28 @@
+/*************************************************************************************************
+* This file is part of the Nebula Logger project, released under the MIT License.                *
+* See LICENSE file or go to https://github.com/jongpie/NebulaLogger for full license details.    *
+*************************************************************************************************/
+({
+    queryLog: function (component, event) {
+        let logId = component.get('v.recordId')
+        console.log('querying log ID: ' + logId);
+
+        let action = component.get('c.getLog');
+        action.setParams({
+            logId : logId
+        });
+        action.setCallback(this, function (response) {
+            if (response.getState() === 'SUCCESS') {
+                let log = response.getReturnValue();
+                component.set('v.log', log);
+
+                var logJSON = JSON.stringify(log, null, '\t');
+                console.log('logJSON=' + logJSON);
+                component.set('v.logJSON', logJSON);
+            } else if (response.getState() === 'ERROR') {
+                // TODO this.processCallbackErrors(response);
+            }
+        });
+        $A.enqueueAction(action);
+    }
+})
