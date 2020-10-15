4eba04255ff67df39262935bb4565940ca791410
James Simone <jamirus99@gmail.com>
2020-10-15 11:28:16 -0600

    Fixing one more bracket, updating test file

--- scripts/findTests.js
+++ scripts/findTests.js
@@ -1,12 +1,12 @@
 const fs = require("fs");
 const path = require("path")
-const dirPath = path.join(__dirname, 'nebula-logger/tests');
+const dirPath = path.join(__dirname, '../nebula-logger/tests');
 
 const testFileNames = [];
 
 function walkSync(currentDirPath) {
   fs.readdirSync(currentDirPath).forEach(function (name) {
-      if(name.endsWith('-meta.xml')) continue;
+      if(name.endsWith('-meta.xml')) return;
       const filePath = path.join(currentDirPath, name);
       const stat = fs.statSync(filePath);
       if (stat.isFile() && name.indexOf('Test') > -1) {
