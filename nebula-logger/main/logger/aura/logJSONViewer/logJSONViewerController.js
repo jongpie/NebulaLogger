0ca6ce23bd269a47f02d6efa75ab84ae087669ce
Jonathan Gillespie <jonathan.c.gillespie@gmail.com>
2020-10-14 23:37:34 -0700

    Added footer with 'Copy JSON to Clipboard' button

--- nebula-logger/main/logger/aura/logJSONViewer/logJSONViewerController.js
+++ nebula-logger/main/logger/aura/logJSONViewer/logJSONViewerController.js
@@ -5,5 +5,23 @@
 ({
     doInit : function(component, event, helper) {
         helper.queryLog(component, event);
+    },
+    copyToClipboard : function(component, event, helper) {
+        var logJSON = component.get('v.logJSON');
+
+        // Add a hidden input to store the JSON
+        var hiddenJSONInput = document.createElement('input');
+        hiddenJSONInput.setAttribute('value', logJSON);
+        document.body.appendChild(hiddenJSONInput);
+        hiddenJSONInput.select();
+
+        // Copy to clipboard
+        document.execCommand('copy');
+        document.body.removeChild(hiddenJSONInput);
+
+        // Update the button to show that the JSON has been copied
+        event.getSource().set('v.iconName' , 'utility:check');
+        event.getSource().set('v.label', 'JSON Copied');
+        event.getSource().set('v.variant', 'success');
     }
 })
