9f1da3b468cf83e748525a7444a1749fcbb5a3bf
James Simone <16430727+jamessimone@users.noreply.github.com>
2020-10-17 12:41:24 -0600

    Quiddity, RequestId & Uuid deprecation (#28)
    * bump API version for Winter '21
    
    * Added Quiddity/Request objects, removed namespace to prep for package preparation in sfdx-project.json
    
    * Code review feedback, plus minimized some boilerplate using the new Winter21 safe navigation feature
    
    * Removing package, package version name and version number from sfdx-project.json
    
    * Fix for scheduled flows not having access to Auth.SessionManagement, easy to detect with Quiddity!
    
    * Updates after rebasing to latest
    
    * Dynamic forms (#55)
    
    * bump API version for Winter '21
    
    * Added Quiddity/Request objects, removed namespace to prep for package preparation in sfdx-project.json
    
    * Addresses #41. Can't yet delete page layouts because at least one per object is required
    
    * Fixing sfdx-project.json regression
    
    * Fixes #61 and rebase fixes
    
    * PMD fixes. Updates to session Id length field, which either increased in Winter21 or was throwing errors previously without us knowing
    
    * Cleanup

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,3 +1,5 @@
 #/bin/bash
 fileNames=$(cat test-files)
-sfdx force:apex:test:run -w 15 -n $fileNames
+echo "Beginning test run for ${fileNames}"
+# if the job fails, clean up automatically
+sfdx force:apex:test:run -w 15 -n $fileNames || node scripts/findTestsCleanup.js
