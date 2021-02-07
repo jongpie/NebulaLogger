371c394af16c35f112ace3ce362930fbb614d7d7
james simone <jamirus99@gmail.com>
2021-02-07 13:25:03 -0700

    Revert deletion of Aura component and use as wrapper till LWC Quick Actions are out of pilot

--- nebula-logger/main/log-management/lwc/logViewer/logViewer.js
+++ nebula-logger/main/log-management/lwc/logViewer/logViewer.js
@@ -1,15 +1,21 @@
+/*************************************************************************************************
+ * This file is part of the Nebula Logger project, released under the MIT License.                *
+ * See LICENSE file or go to https://github.com/jongpie/NebulaLogger for full license details.    *
+ *************************************************************************************************/
 import { api, LightningElement, wire } from 'lwc';
 import getLog from '@salesforce/apex/Logger.getLog';
 
 export default class LogJSONViewer extends LightningElement {
+    // TODO: recordId is a reserved keyword in LWC that only works if the component has been inserted by means of Flexipage
+    // onto a record page. We can rename this variable (probably) once LWC quick actions are GA'd
     @api
-    recordId;
-    @wire(getLog, { logId: '$recordId' })
+    logId;
+    @wire(getLog, { logId: '$logId' })
     log;
 
     jsonCopied = false;
 
-    get logJson() {
+    get logJSON() {
         return this.log.data ? JSON.stringify(this.log.data, null, '\t') : '';
     }
 
@@ -21,16 +27,11 @@ export default class LogJSONViewer extends LightningElement {
         return this.jsonCopied ? 'success' : 'brand';
     }
 
-    copyToClipboard(value) {
-        // Add a hidden input to store the JSON
-        const hiddenJSONInput = document.createElement('input');
-        hiddenJSONInput.setAttribute('value', value);
-        document.body.appendChild(hiddenJSONInput);
-        hiddenJSONInput.select();
-
-        // Copy to clipboard
-        document.execCommand('copy');
-        document.body.removeChild(hiddenJSONInput);
+    copyToClipboard() {
+        const value = this.template.querySelector('pre').textContent;
+        // I figure it might be nice to also include the parsed JSON in the console
+        console.log(JSON.parse(value));
+        document.execCommand('copy', value);
         this.jsonCopied = true;
     }
 }
