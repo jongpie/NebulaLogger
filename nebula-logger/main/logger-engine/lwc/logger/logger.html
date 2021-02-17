57d8170ce632434257049e3742f1fe1e275a7105
Jonathan Gillespie <jonathan.c.gillespie@gmail.com>
2021-02-16 18:22:37 -0800

    Testing out different approaches for function parameters

--- nebula-logger/main/logger-engine/lwc/logger/logger.html
+++ nebula-logger/main/logger-engine/lwc/logger/logger.html
@@ -1,3 +1,3 @@
 <template>
-    
+    hello
 </template>
--- nebula-logger/main/logger-engine/lwc/logger/logger.js
+++ nebula-logger/main/logger-engine/lwc/logger/logger.js
@@ -1,15 +1,20 @@
 // videoPlayer.js
 import { LightningElement, api } from 'lwc';
 
+const isRequired = () => {
+    throw new Error('Parameter is required');
+};
+
 export default class Logger extends LightningElement {
     @api
-    error(message, recordId, record, topics) {
-        this.newEntry('ERROR', message, recordId, record, topics);
+    error(message = isRequired(), logEntryOptions = { exception: null, recordId: null, record: null, topics: [] }) {
+        this.logNewEntry('ERROR', message, logEntryOptions);
+        //this.newEntry('ERROR', message, recordId, record, topics);
         console.error(message);
     }
 
     @api
-    warn(message, recordId, record, topics) {
+    warn(message = isRequired(), recordId, record, topics) {
         this.newEntry('WARN', message, recordId, record, topics);
         console.warn(message);
     }
@@ -26,6 +31,13 @@ export default class Logger extends LightningElement {
         console.debug(message);
     }
 
+    // TODO consider adding fine, finer and finest, even though JS doesn't really have them
+    // @api
+    // fine(message, recordId, record, topics) {
+    //     this.newEntry('FINE', message, recordId, record, topics);
+    //     console.debug(message);
+    // }
+
     @api
     saveLog() {
         alert('TODO');
@@ -40,10 +52,33 @@ export default class Logger extends LightningElement {
     }
 
     get stackTrace() {
+        // TODO consider adding parsing/cleanup for stack traces
         let err = new Error();
         return err.stack;
     }
 
+    logNewEntry(loggingLevel, message, logEntryOptions) {
+        // TODO add checks to make sure logEntryOptions exists & has all the keys/properties
+
+
+        let stackTrace = stackTrace();
+
+        let entry = {
+            exception : null, // TODO create object for JS errors & SF-specific errors
+            loggingLevel: loggingLevel,
+            message: message,
+            recordId: logEntryOptions.recordId,
+            record: logEntryOptions.record,
+            stackTrace: stackTrace,
+            timestamp: new Date().toISOString(),
+            topics: logEntryOptions.topics
+        };
+        this.entries.push(entry);
+
+        return entry;
+    }
+
+    // TODO delete?
     newEntry(loggingLevel, message, recordId, record, topics) {
         let entry = {
             loggingLevel: loggingLevel,
