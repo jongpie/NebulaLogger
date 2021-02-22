94f0cc77480b6f59ea7862b08846bbf63f2a7546
Jonathan Gillespie <jonathan.c.gillespie@gmail.com>
2021-02-21 20:02:21 -0800

    Updated comments

--- nebula-logger/main/log-management/lwc/relatedLogEntries/relatedLogEntries.js
+++ nebula-logger/main/log-management/lwc/relatedLogEntries/relatedLogEntries.js
@@ -1,3 +1,8 @@
+/*************************************************************************************************
+ * This file is part of the Nebula Logger project, released under the MIT License.                *
+ * See LICENSE file or go to https://github.com/jongpie/NebulaLogger for full license details.    *
+ *************************************************************************************************/
+
 import { LightningElement, api, track, wire } from 'lwc';
 import getQueryResult from '@salesforce/apex/RelatedLogEntriesController.getQueryResult';
 export default class RelatedLogEntries extends LightningElement {
