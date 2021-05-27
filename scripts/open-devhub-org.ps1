8b5d0b18e575fb9b2aaf4b5167fdbe3206d805d8
Jonathan Gillespie <jonathan.c.gillespie@gmail.com>
2021-05-27 00:12:49 -0700

    Added npm run devhub:open and org:open

---
+++ scripts/open-devhub-org.ps1
@@ -0,0 +1,15 @@
+# This is used to easily open a devhub org in your browser
+# If no devhub is specified using the `-devhub` parameter, then the default devhub is used
+param ([string]$devHub)
+
+if (!$devHub) {
+    Write-Output "No dev hub specified, checking for default dev hub instead"
+
+    $defaultDevHub = (sfdx config:get defaultdevhubusername  --json | ConvertFrom-Json).result.value
+    Write-Output "Default Dev Hub: $defaultDevHub"
+
+    $devHub = $defaultDevHub
+}
+
+Write-Output "Opening dev hub: $devHub"
+sfdx force:org:open --targetusername $devHub
