18cd8eb973705375313826376bce5971dcb78d8b
Jonathan Gillespie <jonathan.c.gillespie@gmail.com>
2021-12-10 21:23:00 -0500

    Improved test approach for verifying fields on new record

--- nebula-logger/main/log-management/lwc/loggerSettings/__tests__/loggerSettings.test.js
+++ nebula-logger/main/log-management/lwc/loggerSettings/__tests__/loggerSettings.test.js
@@ -174,47 +174,38 @@ describe('Logger Settings lwc tests', () => {
         const recordModal = loggerSettingsElement.shadowRoot.querySelector('.slds-modal');
         expect(recordModal).toBeTruthy();
 
-        // Check each of the fields within the modal to ensure the default field values are populated
-        // TODO find a way to do this dynamically (using querySelectorAll?) to reduce LOC
+        // Check the picklist fields to ensure the correct options are mapped
         const setupOwnerTypeField = loggerSettingsElement.shadowRoot.querySelector('[data-id="setupOwnerType"]');
-        expect(setupOwnerTypeField).toBeTruthy();
         expect(setupOwnerTypeField.options).toEqual(mockPicklistOptions.setupOwnerTypeOptions);
-        expect(setupOwnerTypeField.value).toBeFalsy();
-        const setupOwnerNameField = loggerSettingsElement.shadowRoot.querySelector('lightning-input[data-id="setupOwnerName"]');
-        expect(setupOwnerNameField).toBeFalsy();
-        const isEnabledField = loggerSettingsElement.shadowRoot.querySelector('[data-id="IsEnabled__c"]');
-        expect(isEnabledField).toBeTruthy();
-        expect(isEnabledField.checked).toEqual(mockNewRecord.IsEnabled__c);
         const loggingLevelField = loggerSettingsElement.shadowRoot.querySelector('[data-id="LoggingLevel__c"]');
-        expect(loggingLevelField).toBeTruthy();
         expect(loggingLevelField.options).toEqual(mockPicklistOptions.loggingLevelOptions);
-        expect(loggingLevelField.value).toEqual(mockNewRecord.LoggingLevel__c);
-        const defaultNumberOfDaysToRetainLogsField = loggerSettingsElement.shadowRoot.querySelector('[data-id="DefaultNumberOfDaysToRetainLogs__c"]');
-        expect(defaultNumberOfDaysToRetainLogsField).toBeTruthy();
-        expect(defaultNumberOfDaysToRetainLogsField.value).toEqual(mockNewRecord.DefaultNumberOfDaysToRetainLogs__c);
         const defaultLogShareAccessLevelField = loggerSettingsElement.shadowRoot.querySelector('[data-id="DefaultLogShareAccessLevel__c"]');
-        expect(defaultLogShareAccessLevelField).toBeTruthy();
         expect(defaultLogShareAccessLevelField.options).toEqual(mockPicklistOptions.shareAccessLevelOptions);
-        expect(defaultLogShareAccessLevelField.value).toEqual(mockNewRecord.DefaultLogShareAccessLevel__c);
         const defaultSaveMethodField = loggerSettingsElement.shadowRoot.querySelector('[data-id="DefaultSaveMethod__c"]');
-        expect(defaultSaveMethodField).toBeTruthy();
         expect(defaultSaveMethodField.options).toEqual(mockPicklistOptions.saveMethodOptions);
-        expect(defaultSaveMethodField.value).toEqual(mockNewRecord.DefaultSaveMethod__c);
-        const isApexSystemDebugLoggingEnabledField = loggerSettingsElement.shadowRoot.querySelector('[data-id="IsApexSystemDebugLoggingEnabled__c"]');
-        expect(isApexSystemDebugLoggingEnabledField).toBeTruthy();
-        expect(isApexSystemDebugLoggingEnabledField.checked).toEqual(mockNewRecord.IsApexSystemDebugLoggingEnabled__c);
-        const isComponentConsoleLoggingEnabledField = loggerSettingsElement.shadowRoot.querySelector('[data-id="IsComponentConsoleLoggingEnabled__c"]');
-        expect(isComponentConsoleLoggingEnabledField).toBeTruthy();
-        expect(isComponentConsoleLoggingEnabledField.checked).toEqual(mockNewRecord.IsComponentConsoleLoggingEnabled__c);
-        const isDataMaskingEnabledField = loggerSettingsElement.shadowRoot.querySelector('[data-id="IsDataMaskingEnabled__c"]');
-        expect(isDataMaskingEnabledField).toBeTruthy();
-        expect(isDataMaskingEnabledField.checked).toEqual(mockNewRecord.IsDataMaskingEnabled__c);
-        const stripInaccessibleRecordFieldsField = loggerSettingsElement.shadowRoot.querySelector('[data-id="StripInaccessibleRecordFields__c"]');
-        expect(stripInaccessibleRecordFieldsField).toBeTruthy();
-        expect(stripInaccessibleRecordFieldsField.checked).toEqual(mockNewRecord.StripInaccessibleRecordFields__c);
-        const isAnonymousModeEnabled__cField = loggerSettingsElement.shadowRoot.querySelector('[data-id="IsAnonymousModeEnabled__c"]');
-        expect(isAnonymousModeEnabled__cField).toBeTruthy();
-        expect(isAnonymousModeEnabled__cField.checked).toEqual(mockNewRecord.IsAnonymousModeEnabled__c);
+
+        // Check each of the fields within the modal to ensure the default field values are populated
+        const expectedFieldNames = [
+            'IsEnabled__c',
+            'LoggingLevel__c',
+            'DefaultNumberOfDaysToRetainLogs__c',
+            'DefaultLogShareAccessLevel__c',
+            'DefaultSaveMethod__c',
+            'IsApexSystemDebugLoggingEnabled__c',
+            'IsComponentConsoleLoggingEnabled__c',
+            'IsDataMaskingEnabled__c',
+            'StripInaccessibleRecordFields__c',
+            'IsAnonymousModeEnabled__c'
+        ];
+        expectedFieldNames.forEach(fieldName => {
+            const inputField = loggerSettingsElement.shadowRoot.querySelector('[data-id="' + fieldName + '"]');
+            expect(inputField).toBeTruthy();
+            if (inputField.checked !== undefined) {
+                expect(inputField.checked).toBe(mockNewRecord[fieldName]);
+            } else {
+                expect(inputField.value).toBe(mockNewRecord[fieldName]);
+            }
+        });
     });
 
     it("closes record modal when 'cancel' button is clicked", async () => {
