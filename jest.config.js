95c60aca7ffe6d1fd9d348fec906d44b0e3d279c
Jonathan Gillespie <jonathan.c.gillespie@gmail.com>
2021-12-16 17:50:16 -0500

    Consolidated several top-level directories & files to all be under the directory `config`

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,9 @@
-const { jestConfig } = require('@salesforce/sfdx-lwc-jest/config');
+const { jestConfig } = require("@salesforce/sfdx-lwc-jest/config");
 module.exports = {
-    ...jestConfig,
-    moduleNameMapper: {
-        '^lightning/empApi$': '<rootDir>/nebula-logger/tests/common/jest-mocks/lightning/empApi'
-    }
+  ...jestConfig,
+  moduleNameMapper: {
+    "^lightning/empApi$":
+      "<rootDir>/config/jest/mocks/lightning/empApi",
+  },
+  modulePathIgnorePatterns: ["recipes"],
 };
