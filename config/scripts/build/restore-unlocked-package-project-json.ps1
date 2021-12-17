773f44a8a6973a978d69a657f12fb7447f53344d
Jonathan Gillespie <jonathan.c.gillespie@gmail.com>
2021-12-17 14:10:39 -0500

    Readded some deprecated metadata to the managed-package folder, and added deploy action for generating a beta managed package version Currently, deprecating metadata isn't available for managed packages (unless you opt in to a pilot feature, and I have not done so yet)

--- config/scripts/build/restore-unlocked-package-project-json.ps1
+++ config/scripts/build/restore-unlocked-package-project-json.ps1
@@ -3,11 +3,11 @@
 
 $rootProject = "./sfdx-project.json"
 $unlockedProject = "./unlocked-sfdx-project.json"
-$managedProject = "./managed-package/sfdx-project.json"
+$managedProject = "./nebula-logger/managed-package/sfdx-project.json"
 
 Write-Output "Restoring unlocked package's version of sfdx-project.json"
 Write-Output "Copying $rootProject to $managedProject"
 Copy-Item -Path $rootProject -Destination $managedProject -Force
-Write-Output "Ovewriting $rootProject with $unlockedProject"
+Write-Output "Overwriting $rootProject with $unlockedProject"
 Copy-Item -Path $unlockedProject -Destination $rootProject -Force
 Remove-Item -Path $unlockedProject
--- config/scripts/build/switch-to-managed-package-project-json.ps1
+++ config/scripts/build/switch-to-managed-package-project-json.ps1
@@ -3,10 +3,10 @@
 
 $rootProject = "./sfdx-project.json"
 $unlockedProject = "./unlocked-sfdx-project.json"
-$managedProject = "./managed-package/sfdx-project.json"
+$managedProject = "./nebula-logger/managed-package/sfdx-project.json"
 
 Write-Output "Switching to managed package's version of sfdx-project.json"
 Write-Output "Copying $rootProject to $unlockedProject"
 Copy-Item -Path $rootProject -Destination $unlockedProject -Force
-Write-Output "Ovewriting $rootProject with $managedProject"
+Write-Output "Overwriting $rootProject with $managedProject"
 Copy-Item -Path $managedProject -Destination $rootProject -Force
