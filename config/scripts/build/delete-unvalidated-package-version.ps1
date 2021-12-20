01231feb9c330acd28c73bbfd6b69c348ac376a9
Jonathan Gillespie <jonathan.c.gillespie@gmail.com>
2021-12-20 12:38:16 -0500

    Removed some old pwsh scripts, renamed a few others

--- config/scripts/build/delete-unvalidated-package-version.ps1
+++
@@ -1,4 +0,0 @@
-# This script is used to deleted an unvalidated package version, based on the contents of ./unvalidated-package-version-id.txt
-$unvalidatedPackageVersionId = Get-Content -Path ./unvalidated-package-version-id.txt
-Write-Output "Unvalidated Package Version ID to Delete: $unvalidatedPackageVersionId"
-npx sfdx force:package:version:delete --noprompt --package $unvalidatedPackageVersionId
