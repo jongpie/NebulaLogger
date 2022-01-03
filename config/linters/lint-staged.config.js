4bc96fb1dae50110f6ba5b12f0ce082b26cba8d8
Jonathan Gillespie <jonathan.c.gillespie@gmail.com>
2022-01-03 16:44:28 -0500

    Moved 'streaming/paused' btn in logEntryEventStream.html, fixed eslint in lint-staged.config.js

--- config/linters/lint-staged.config.js
+++ config/linters/lint-staged.config.js
@@ -4,7 +4,7 @@ module.exports = {
     },
     '*.{cls,cmp,component,css,html,js,json,md,page,trigger,yaml,yml}': filenames => filenames.map(filename => `prettier --write '${filename}'`),
     '**/lwc/**': filenames => {
-        return [`eslint ${filenames.join(' ')} --fix`, `npm run test:lwc`];
+        return [`eslint --config ./config/linters/.eslintrc.json ${filenames.join(' ')} --fix`, `npm run test:lwc`];
     },
     '*.{cls,trigger}': () => {
         return [`npm run lint:verify:apex`, `npm run docs:fix && git add ./docs/ && git commit --amend --no-edit`];
