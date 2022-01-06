df364de66edd3246c776037b36133ab075618b90
Jonathan Gillespie <jonathan.c.gillespie@gmail.com>
2022-01-06 00:05:27 -0500

    WIP: Started new lwc loggerInfo to show details about Logger & any installed plugins, added some prototype changes to LoggerPlugin__mdt

---
+++ nebula-logger/core/main/log-management/lwc/loggerInfo/loggerInfo.js
@@ -0,0 +1,34 @@
+/*************************************************************************************************
+ * This file is part of the Nebula Logger project, released under the MIT License.               *
+ * See LICENSE file or go to https://github.com/jongpie/NebulaLogger for full license details.   *
+ ************************************************************************************************/
+
+import { LightningElement } from 'lwc';
+
+// Apex methods
+import getVersionNumber from '@salesforce/apex/LoggerInfoController.getVersionNumber';
+import getNamespacePrefix from '@salesforce/apex/LoggerInfoController.getNamespacePrefix';
+import getPlugins from '@salesforce/apex/LoggerInfoController.getPlugins';
+
+export default class LoggerSystemInfo extends LightningElement {
+    versionNumber;
+    namespacePrefix;
+    plugins = [];
+
+    connectedCallback() {
+        Promise.all([getVersionNumber(), getNamespacePrefix(), getPlugins()]).then(([versionNumber, namespacePrefix, plugins]) => {
+            this.versionNumber = versionNumber;
+            this.namespacePrefix = namespacePrefix;
+            this.plugins = plugins;
+        });
+    }
+
+    get hasPlugins() {
+        return this.plugins && this.plugins.length > 0;
+    }
+
+    viewReleaseNotes() {
+        const releaseNotesUrl = 'https://github.com/jongpie/NebulaLogger/releases/tag/' + this.versionNumber;
+        window.open(releaseNotesUrl);
+    }
+}
