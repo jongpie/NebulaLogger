031d6bf2332f98ce9af3d555a867c9ef34e5b73c
Jonathan Gillespie <jonathan.c.gillespie@gmail.com>
2022-01-17 23:43:54 -0500

    Fixes #271 - Updated LoggerTestUtils to lazy-load standard user profile, using all possible translated names to ensure it works, regardless of the current user's specified language

--- config/scripts/build/generate-lwc-docs.ps1
+++ config/scripts/build/generate-lwc-docs.ps1
@@ -1,6 +1,6 @@
 # This script is used to generate the markdown files used by Github pages for lightning component jsdocs
-npx jsdoc2md nebula-logger/main/logger-engine/lwc/logger/logger.js > ./docs/lightning-components/Logger.md
-npx jsdoc2md nebula-logger/main/logger-engine/lwc/logger/logEntryBuilder.js > ./docs/lightning-components/LogEntryBuilder.md
+npx jsdoc2md nebula-logger/core/main/logger-engine/lwc/logger/logger.js > ./docs/lightning-components/Logger.md
+npx jsdoc2md nebula-logger/core/main/logger-engine/lwc/logger/logEntryBuilder.js > ./docs/lightning-components/LogEntryBuilder.md
 
 $docsSubdirectories = "docs/lightning-components/*.*"
 foreach($file in Get-ChildItem $docsSubdirectories) {
