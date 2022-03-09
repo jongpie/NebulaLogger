362681bc28991ef80a1b2a2fdb647aa5bcabd917
Jonathan Gillespie <jonathan.c.gillespie@gmail.com>
2022-03-09 13:30:29 -0500

    Scope creep (again): renamed logJSON lwc back to logViewer, renamed "ViewJSON" quickAction to "OpenViewer", and added tabs to logViewer lwc to provide tabs for both JSON (existing) and log file (new), plus a new download button to export the content to a file

--- nebula-logger/managed-package/core/main/deprecated/lwc/logViewer/logViewer.js
+++
@@ -1,8 +0,0 @@
-/*************************************************************************************************
- * This file is part of the Nebula Logger project, released under the MIT License.               *
- * See LICENSE file or go to https://github.com/jongpie/NebulaLogger for full license details.   *
- ************************************************************************************************/
-
-import { LightningElement } from 'lwc';
-
-export default class LogViewer extends LightningElement {}
