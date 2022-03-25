b3a15a77b21cbe3018e7fb5df5c388bfcfe3eff3
Jonathan Gillespie <jonathan.c.gillespie@gmail.com>
2022-03-25 15:49:34 -0400

    Got basic tests passing for LogEntryArchiveController.cls and logEntryArchive LWC

--- nebula-logger/plugins/big-object-archiving/plugin/lwc/logEntryArchives/logEntryArchives.js
+++ nebula-logger/plugins/big-object-archiving/plugin/lwc/logEntryArchives/logEntryArchives.js
@@ -1,11 +1,19 @@
 import { LightningElement } from 'lwc';
+import getLogEntryArchives from '@salesforce/apex/LogEntryArchiveController.getLogEntryArchives';
 
 export default class LogEntryArchives extends LightningElement {
+    logEntryArchives;
+
     get title() {
         return 'Log Entry Archives';
     }
 
     async connectedCallback() {
         document.title = this.title;
+
+        getLogEntryArchives().then(results => {
+            this.logEntryArchives = JSON.parse(JSON.stringify(results));
+            console.info('Loaded LogEntryArchive__b records', this.logEntryArchives);
+        });
     }
 }
