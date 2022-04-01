7bea700fe1b64e34f6fa508b4cdc9e958e9ffdd9
Jonathan Gillespie <jonathan.c.gillespie@gmail.com>
2022-03-31 21:24:45 -0400

    Re-arranged test util classes (again) into LoggerTestMockCreator for generic mock & data creation (part of configuration layer), and LoggerTestConfigurator for Nebula Logger-specific mocking & setup (part of the log-management layer)

--- scripts/build/generate-apex-docs.ps1
+++ scripts/build/generate-apex-docs.ps1
@@ -1,18 +1,17 @@
 # This script is used to generate the markdown files used by Github pages for Apex docs
 find ./docs/apex/ -maxdepth 2 -type f -name "*.md" -delete
 
-npx apexdocs-generate --configPath ./config/docs/apexdocs.json --scope global public --sourceDir ./nebula-logger/core/main/ ./nebula-logger/plugins/ --targetDir ./docs/apex --targetGenerator jekyll
+npx apexdocs-generate --configPath ./config/docs/apexdocs.json --scope global public --sourceDir ./nebula-logger/core/ ./nebula-logger/plugins/ --targetDir ./docs/apex --targetGenerator jekyll
 
 # Make a few adjustments to the generated markdown files so that they work correctly in Github Pages
 $indexPageFile = "docs/apex/index.md"
 Write-Output "Processing file: $indexPageFile"
 (Get-Content -path $indexPageFile -Raw) -replace "# Classes","# Nebula Logger for Apex" | Set-Content -Path $indexPageFile -NoNewline
-(Get-Content -path $indexPageFile -Raw) -replace ".md","" | Set-Content -Path $indexPageFile -NoNewline
-(Get-Content -path $indexPageFile -Raw) -replace "/Configuration/","configuration/" | Set-Content -Path $indexPageFile -NoNewline
-(Get-Content -path $indexPageFile -Raw) -replace "/Logger-Engine/","logger-engine/" | Set-Content -Path $indexPageFile -NoNewline
-(Get-Content -path $indexPageFile -Raw) -replace "/Log-Management/","log-management/" | Set-Content -Path $indexPageFile -NoNewline
-(Get-Content -path $indexPageFile -Raw) -replace "/Plugin-Framework/","plugin-framework/" | Set-Content -Path $indexPageFile -NoNewline
-(Get-Content -path $indexPageFile -Raw) -replace "/Plugins/","plugins/" | Set-Content -Path $indexPageFile -NoNewline
+# (Get-Content -path $indexPageFile -Raw) -replace ".md","" | Set-Content -Path $indexPageFile -NoNewline
+# (Get-Content -path $indexPageFile -Raw) -replace "/Configuration/","configuration/" | Set-Content -Path $indexPageFile -NoNewline
+# (Get-Content -path $indexPageFile -Raw) -replace "/Logger-Engine/","logger-engine/" | Set-Content -Path $indexPageFile -NoNewline
+# (Get-Content -path $indexPageFile -Raw) -replace "/Log-Management/","log-management/" | Set-Content -Path $indexPageFile -NoNewline
+# (Get-Content -path $indexPageFile -Raw) -replace "/Plugins/","plugins/" | Set-Content -Path $indexPageFile -NoNewline
 
 $docsSubdirectories = "docs/apex/*/*.*"
 foreach($file in Get-ChildItem $docsSubdirectories) {
@@ -21,11 +20,9 @@ foreach($file in Get-ChildItem $docsSubdirectories) {
     (Get-Content -path $file -Raw) -replace "../Configuration/","" | Set-Content -Path $file -NoNewline
     (Get-Content -path $file -Raw) -replace "../Logger-Engine/","" | Set-Content -Path $file -NoNewline
     (Get-Content -path $file -Raw) -replace "../Log-Management/","" | Set-Content -Path $file -NoNewline
-    (Get-Content -path $file -Raw) -replace "../Plugin-Framework/","" | Set-Content -Path $file -NoNewline
 }
 
-mv ./docs/apex/Configuration/ ./docs/apex/configuration/
-mv ./docs/apex/Logger-Engine/ ./docs/apex/logger-engine/
-mv ./docs/apex/Log-Management/ ./docs/apex/log-management/
-mv ./docs/apex/Plugin-Framework/ ./docs/apex/plugin-framework/
+# mv ./docs/apex/Configuration/ ./docs/apex/configuration/
+# mv ./docs/apex/Logger-Engine/ ./docs/apex/logger-engine/
+# mv ./docs/apex/Log-Management/ ./docs/apex/log-management/
 prettier ./docs/apex/ --write
