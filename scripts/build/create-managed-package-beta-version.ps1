dd1d8039e484bcec134e414506aa5d40b71833e6
Jonathan Gillespie <jonathan.c.gillespie@gmail.com>
2023-03-23 19:30:15 -0400

    Reverted to using a separate sfdx-project.json file for the managed package (namespaces are the worst), and updated some package.json scripts to use the new sfdx syntax for assigning permission sets

--- scripts/build/create-managed-package-beta-version.ps1
+++ scripts/build/create-managed-package-beta-version.ps1
@@ -1,22 +1,32 @@
-# There is some deprecated metadata that only lives in the managed package
-# This script handles combining the managed package's deprecated metadata with
-# the core unlocked package's metadata, and then creates a new package version
+# There are 2 versions of sfdx-project.json - 1 for the unlocked package and 1 for the managed package
+# This script handles switching to the managed package's version of the file, and creating a beta of the managed package
 
-$sfdxProjectJsonPath = "./sfdx-project.json"
+$rootProject = "./sfdx-project.json"
+$unlockedProject = "./unlocked-sfdx-project.json"
+$managedProject = "./nebula-logger/managed-package/sfdx-project.json"
 
-Write-Output "Copying metadata in ./nebula-logger/core/ to ./nebula-logger/managed-package/"
-cp --recursive --force ./nebula-logger/core/ ./nebula-logger/managed-package/
+# Switch to using the managed package's sfdx-project.json file
+Write-Output "Switching to managed package's version of sfdx-project.json"
+Write-Output "Copying $rootProject to $unlockedProject"
+Copy-Item -Path $rootProject -Destination $unlockedProject -Force
+Write-Output "Overwriting $rootProject with $managedProject"
+Copy-Item -Path $managedProject -Destination $rootProject -Force
 
 # Create a new version of the managed package
-Write-Output "Creating new package version for the managed package"
+cp -R ./nebula-logger/core/ ./nebula-logger/managed-package/
 $gitBranch = (git branch --show-current)
 $gitCommit = (git rev-parse HEAD)
 npx sfdx package version create --json --package "Nebula Logger - Managed Package" --code-coverage --installation-key-bypass --wait 60 --branch $gitBranch --tag $gitCommit
-$packageVersionCreateExitCode = $LASTEXITCODE
-git clean -df -- nebula-logger/managed-package/core
-if ($packageVersionCreateExitCode -ne 0) {
+if ($LASTEXITCODE -ne 0) {
     throw "Error creating package version for managed package"
 }
+git clean -df -- nebula-logger/managed-package/core
 
-Write-Output "Formatting $sfdxProjectJsonPath with prettier"
-npx prettier --write $sfdxProjectJsonPath
+# Restore the sfdx-project.json files to their original locations
+Write-Output "Restoring unlocked package's version of sfdx-project.json"
+Write-Output "Copying $rootProject to $managedProject"
+Copy-Item -Path $rootProject -Destination $managedProject -Force
+npx prettier --write $managedProject
+Write-Output "Overwriting $rootProject with $unlockedProject"
+Copy-Item -Path $unlockedProject -Destination $rootProject -Force
+Remove-Item -Path $unlockedProject
