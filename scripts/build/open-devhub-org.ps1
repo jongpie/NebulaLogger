56f2d3bb874a35390717442f269e0b42a095332f
jmercie <themercie@gmail.com>
2023-07-30 20:33:48 -0300

    feat:update pwsh scripts to use sf cli

--- scripts/build/open-devhub-org.ps1
+++ scripts/build/open-devhub-org.ps1
@@ -5,11 +5,11 @@ param ([string]$devHub)
 if (!$devHub) {
     Write-Output "No dev hub specified, checking for default dev hub instead"
 
-    $defaultDevHub = (npx sfdx config:get defaultdevhubusername  --json | ConvertFrom-Json).result.value
+    $defaultDevHub = (npx sf config get target-dev-hub  --json | ConvertFrom-Json).result.value
     Write-Output "Default Dev Hub: $defaultDevHub"
 
     $devHub = $defaultDevHub
 }
 
 Write-Output "Opening dev hub: $devHub"
-npx sfdx force:org:open --targetusername $devHub
+npx sf org open --target-org $devHub
