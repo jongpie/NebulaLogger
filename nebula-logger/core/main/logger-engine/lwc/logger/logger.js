41e6594c3c33683d9397558a2fd51a726970da79
patrick-skamarak <85705988+patrick-skamarak@users.noreply.github.com>
2023-09-19 10:35:54 -0400

    Bugfix for unhandled exception when email limits are reached (#548)
    * Added catch for System.HandledException in LoggerEmailSender.cls to avoid trying to send emails when the org's single email limit has been exceeded

--- nebula-logger/core/main/logger-engine/lwc/logger/logger.js
+++ nebula-logger/core/main/logger-engine/lwc/logger/logger.js
@@ -6,7 +6,7 @@
 import { LightningElement, api } from 'lwc';
 import { createLoggerService } from './loggerService';
 
-const CURRENT_VERSION_NUMBER = 'v4.11.2';
+const CURRENT_VERSION_NUMBER = 'v4.11.3';
 
 export default class Logger extends LightningElement {
     #loggerService = createLoggerService();
