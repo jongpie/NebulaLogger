b64a300f446a2871ce0e334432ac30b13e07a711
Jonathan Gillespie <jonathan.c.gillespie@gmail.com>
2023-10-24 21:34:56 -0400

    Uhhhh... How does this work again?

--- docs/conf.py
+++ docs/conf.py
@@ -50,9 +50,9 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "sphinx_rtd_theme"
+# html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
+# html_static_path = ["_static"]
