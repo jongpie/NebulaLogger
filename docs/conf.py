e8edcda23bed808fa1f469020c286817802ec4e7
Jonathan Gillespie <jonathan.c.gillespie@gmail.com>
2023-10-25 23:18:47 -0400

    Hmmm

--- docs/conf.py
+++ docs/conf.py
@@ -57,11 +57,10 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-# html_theme = 'sphinx_rtd_theme'
-html_theme = 'pydata-sphinx-theme'
-html_theme_options = {
-    'navigation_depth': 4,
-}
+html_theme = 'sphinx_rtd_theme'
+# html_theme_options = {
+#     'navigation_depth': 4,
+# }
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
