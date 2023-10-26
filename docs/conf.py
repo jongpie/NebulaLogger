41dc29c6b3695cb9956a5f0afc1790202330123a
Jonathan Gillespie <jonathan.c.gillespie@gmail.com>
2023-10-26 15:55:13 -0400

    Hmmm

--- docs/conf.py
+++ docs/conf.py
@@ -23,25 +23,26 @@ author = "Jonathan Gillespie"
 
 
 # -- General configuration ---------------------------------------------------
-# -- General configuration
 
 # extensions = ["sphinx.ext.autodoc", "sphinxcontrib.httpdomain", "myst_parser", "sphinx_rtd_theme"]
 extensions = [
-    'sphinx.ext.duration',
-    'sphinx.ext.doctest',
+    'myst_parser',
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
+    'sphinx.ext.doctest',
+    'sphinx.ext.duration',
     'sphinx.ext.intersphinx',
 ]
 
-intersphinx_mapping = {
-    "rtd": ("https://docs.readthedocs.io/en/stable/", None),
-    "python": ("https://docs.python.org/3/", None),
-    "sphinx": ("https://www.sphinx-doc.org/en/master/", None),
-}
-intersphinx_disabled_domains = ["std"]
+# intersphinx_mapping = {
+#     "rtd": ("https://docs.readthedocs.io/en/stable/", None),
+#     "python": ("https://docs.python.org/3/", None),
+#     "sphinx": ("https://www.sphinx-doc.org/en/master/", None),
+# }
+# intersphinx_disabled_domains = ["std"]
 
-source_suffix = [".rst", ".md"]
+# source_suffix = [".rst", ".md"]
+source_suffix = {".rst": "restructuredtext", ".md": "markdown"}
 templates_path = ["_templates"]
 
 # -- Options for EPUB output
@@ -61,10 +62,46 @@ html_theme = 'sphinx_rtd_theme'
 html_theme_options = {
     'collapse_navigation': False,
     'navigation_depth': 4,
-    'prev_next_buttons_location': 'both'
+    'prev_next_buttons_location': 'both',
+    'style_external_links': True
 }
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
+# html_static_path = ["_static"]
+
+
+############
+#### TODO hmmmmm
+# extensions = ["sphinx.ext.autodoc", "sphinxcontrib.httpdomain", "myst_parser"]
+
+# intersphinx_mapping = {
+#     "rtd": ("https://docs.readthedocs.io/en/stable/", None),
+#     "python": ("https://docs.python.org/3/", None),
+#     "sphinx": ("https://www.sphinx-doc.org/en/master/", None),
+# }
+# intersphinx_disabled_domains = ["std"]
+
+# source_suffix = [".rst", ".md"]
+# templates_path = ["_templates"]
+
+# # -- Options for EPUB output
+# epub_show_urls = "footnote"
+
+# # List of patterns, relative to source directory, that match files and
+# # directories to ignore when looking for source files.
+# # This pattern also affects html_static_path and html_extra_path.
+# exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+# # -- Options for HTML output -------------------------------------------------
+
+# # The theme to use for HTML and HTML Help pages.  See the documentation for
+# # a list of builtin themes.
+# #
+# html_theme = "sphinx_rtd_theme"
+
+# # Add any paths that contain custom static files (such as style sheets) here,
+# # relative to this directory. They are copied after the builtin static files,
+# # so a file named "default.css" will overwrite the builtin "default.css".
 # html_static_path = ["_static"]
