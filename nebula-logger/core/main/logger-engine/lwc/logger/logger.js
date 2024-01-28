d452edd7fc92ab9411aea24cfb6e7981e5fd44fa
Jonathan Gillespie <jonathan.c.gillespie@gmail.com>
2024-01-28 12:34:46 -0500

    Bugfix for broken LWC stack trace parsing (#614)
    * Fixed ComponentLogger.cls so it handles recent-ish changes to the format of stack traces in LWCs
    
    * Suppressed a 'PropertyNamingConventions' PMD violation in ComponentLogger.cls due to a new lazy-loaded constant
    
    * Also fixed a System.debug() call in Logger.cls to just use 'Organization' instead of 'Schema.Organization' (caused by an overeager string replacement in a previous release)

--- nebula-logger/core/main/logger-engine/lwc/logger/logger.js
+++ nebula-logger/core/main/logger-engine/lwc/logger/logger.js
@@ -6,7 +6,7 @@
 import { LightningElement, api } from 'lwc';
 import { createLoggerService } from './loggerService';
 
-const CURRENT_VERSION_NUMBER = 'v4.12.6';
+const CURRENT_VERSION_NUMBER = 'v4.12.7';
 
 export default class Logger extends LightningElement {
     #loggerService = createLoggerService();
