5693bb298f7c0142569b7a6d5997a1962603eef1
Jonathan Gillespie <jonathan.c.gillespie@gmail.com>
2024-02-08 08:23:27 -0500

    Bumped package version number, name, and description

--- nebula-logger/core/main/logger-engine/lwc/logger/logger.js
+++ nebula-logger/core/main/logger-engine/lwc/logger/logger.js
@@ -6,7 +6,7 @@
 import { LightningElement, api } from 'lwc';
 import { createLoggerService } from './loggerService';
 
-const CURRENT_VERSION_NUMBER = 'v4.12.9';
+const CURRENT_VERSION_NUMBER = 'v4.13.0';
 
 export default class Logger extends LightningElement {
     #loggerService = createLoggerService();
