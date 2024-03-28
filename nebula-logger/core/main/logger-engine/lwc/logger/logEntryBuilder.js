bda728222a0fe50d9a75911c1bf196b14f376a90
James Simone <16430727+jamessimone@users.noreply.github.com>
2024-03-28 12:45:01 -0400

    Bumping version number, name, and description

--- nebula-logger/core/main/logger-engine/lwc/logger/logEntryBuilder.js
+++ nebula-logger/core/main/logger-engine/lwc/logger/logEntryBuilder.js
@@ -4,7 +4,7 @@
 //------------------------------------------------------------------------------------------------//
 import FORM_FACTOR from '@salesforce/client/formFactor';
 
-const CURRENT_VERSION_NUMBER = 'v4.13.4';
+const CURRENT_VERSION_NUMBER = 'v4.13.5';
 
 // JavaScript equivalent to the Apex class ComponentLogger.ComponentLogEntry
 const ComponentLogEntry = class {
