ef1a0569b731669a6404330f8cbf597ec5c1c46f
ZackFra <zack11frazier22@gmail.com>
2024-05-26 12:23:15 -0700

    Got rid of "createLogger" on component, found it wasn't needed now.

--- nebula-logger/recipes/aura/loggerAuraDemo/loggerAuraDemoController.js
+++ nebula-logger/recipes/aura/loggerAuraDemo/loggerAuraDemoController.js
@@ -3,7 +3,7 @@
     saveLogExample: function (component) {
         console.log("start of aura cmp's saveLog function");
 
-        const logger = component.find('logger').createLogger();
+        const logger = component.find('logger');
         console.log(logger);
         logger.fine('Logging from an aura component').addTag('aura cmp');
         const entry = logger.info(component.get('{!v.logMessage}'));
