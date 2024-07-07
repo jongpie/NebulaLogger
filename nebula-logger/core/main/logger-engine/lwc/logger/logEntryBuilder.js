88c630bce5d4ab128316dc2795e12518bcac95e4
Andrii Solokh <asolokh@gmail.com>
2024-07-07 21:28:51 +0300

    Added relatedLogEntries refresh button + spinner (#703)
    * Added refresh button and spinner to relatedLogEntries LWC
    
    * Added sample Account flexipage w/ relatedLogEntries LWC to make it easy to test in a scratch org

--- nebula-logger/core/main/logger-engine/lwc/logger/logEntryBuilder.js
+++ nebula-logger/core/main/logger-engine/lwc/logger/logEntryBuilder.js
@@ -4,7 +4,7 @@
 //------------------------------------------------------------------------------------------------//
 import FORM_FACTOR from '@salesforce/client/formFactor';
 
-const CURRENT_VERSION_NUMBER = 'v4.13.11';
+const CURRENT_VERSION_NUMBER = 'v4.13.12';
 
 // JavaScript equivalent to the Apex class ComponentLogger.ComponentLogEntry
 const ComponentLogEntry = class {
