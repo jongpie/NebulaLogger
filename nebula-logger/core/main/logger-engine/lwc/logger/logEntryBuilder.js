91f2eb2a9b99cf99d51672000b58c463e056c734
Jonathan Gillespie <jonathan.c.gillespie@gmail.com>
2024-07-31 12:59:58 +0000

    Bugfixes for Scenario Rules (#722)
    * Fixed #538 by changing LogEntryEventHandler to always save log entry events when LoggerSettings__c.DefaultPlatformEventStorageLoggingLevel__c is null. When previously using the user's logging level from LoggerSettings__c as a fallback value, it could result in entries being lost if a matching LoggerScenarioRule__mdt exists with a lower logging level
    
    * Fixed some unreported issues in Logger with setScenario() and endScenario() not properly updating the field values returned by getUserSettings(). Now, both setScenario() and endScenario() wipe out & reload the in-memory instance of LoggerSettings__c (before applying any matching LoggerScenarioRule__mdt records) to ensure that there are not any remnants lingering when multiple LoggerScenarioRule__mdt records have been applied in a single transaction

--- nebula-logger/core/main/logger-engine/lwc/logger/logEntryBuilder.js
+++ nebula-logger/core/main/logger-engine/lwc/logger/logEntryBuilder.js
@@ -4,7 +4,7 @@
 //------------------------------------------------------------------------------------------------//
 import FORM_FACTOR from '@salesforce/client/formFactor';
 
-const CURRENT_VERSION_NUMBER = 'v4.13.15';
+const CURRENT_VERSION_NUMBER = 'v4.13.16';
 
 // JavaScript equivalent to the Apex class ComponentLogger.ComponentLogEntry
 const ComponentLogEntry = class {
