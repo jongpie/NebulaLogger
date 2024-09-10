081e23a675ceecf9f4763e87824d9c4b69eb9fb5
Jonathan Gillespie <jonathan.c.gillespie@gmail.com>
2024-09-09 23:26:03 -0400

    [WIP]

--- config/scratch-orgs/omnistudio/setup.sh
+++
@@ -1,9 +0,0 @@
-# https://github.com/SFDC-Assets/omnistudio-basic-scratchorg/blob/master/orgInit.sh
-
-# sf project deploy start --source-dir ./metadata/
-
-PermissionSet permissionSet = [SELECT Id FROM PermissionSet WHERE Name = 'OmniStudioAdmin'];
-insert new Schema.PermissionSetAssignment(
-  AssigneeId = System.UserInfo.getUserId(),
-  PermissionSetId = permissionSet.Id
-);
