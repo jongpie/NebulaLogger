ef8c1ed7777ac2e2a47a13d8585bc06d4503355b
Jonathan Gillespie <jonathan.c.gillespie@gmail.com>
2024-10-27 14:25:36 -0400

    Added more details in logger LWC to the component log entry JSON that's printed using console statements
    The stringified object now includes more details, such as the exception and tags
    
    Calling the console function now happens with a delay (using setTimeout()) so that additional details can be added to the log entry (using the builder methods) before the log entry is stringified & printed out

--- nebula-logger/recipes/lwc/loggerLWCGetLoggerImportDemo/loggerLWCGetLoggerImportDemo.js
+++ nebula-logger/recipes/lwc/loggerLWCGetLoggerImportDemo/loggerLWCGetLoggerImportDemo.js
@@ -19,6 +19,7 @@ export default class LoggerLWCGetLoggerImportDemo extends LightningElement {
 
   connectedCallback() {
     try {
+      this.logger.setScenario(this.scenario);
       this.logger.error('test error entry').setField({ SomeLogEntryField__c: 'some text from loggerLWCGetLoggerImportDemo' });
       this.logger.warn('test warn entry').setField({ SomeLogEntryField__c: 'some text from loggerLWCGetLoggerImportDemo' });
       this.logger.info('test info entry').setField({ SomeLogEntryField__c: 'some text from loggerLWCGetLoggerImportDemo' });
@@ -83,6 +84,7 @@ export default class LoggerLWCGetLoggerImportDemo extends LightningElement {
 
   scenarioChange(event) {
     this.scenario = event.target.value;
+    this.logger.setScenario(this.scenario);
   }
 
   tagsStringChange(event) {
@@ -157,7 +159,6 @@ export default class LoggerLWCGetLoggerImportDemo extends LightningElement {
 
   saveLogExample() {
     console.log('running saveLog for btn');
-    this.logger.setScenario(this.scenario);
     console.log(this.logger);
     // this.logger.saveLog('QUEUEABLE');
     this.logger.saveLog();
