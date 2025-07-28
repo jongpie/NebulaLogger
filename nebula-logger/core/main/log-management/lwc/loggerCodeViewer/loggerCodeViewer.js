4f40d71fe14ec787f69cdcfa02e3d4e9ad64d526
Jonathan Gillespie <jonathan.c.gillespie@gmail.com>
2025-07-28 10:53:00 -0400

    'Finished' fixing code highlightning in loggerCodeViewer

--- nebula-logger/core/main/log-management/lwc/loggerCodeViewer/loggerCodeViewer.js
+++ nebula-logger/core/main/log-management/lwc/loggerCodeViewer/loggerCodeViewer.js
@@ -15,8 +15,6 @@ export default class LoggerCodeViewer extends LightningElement {
 
   isLoaded = false;
 
-  _Prism;
-
   async renderedCallback() {
     if (this.isLoaded) {
       return;
@@ -32,60 +30,58 @@ export default class LoggerCodeViewer extends LightningElement {
     // eslint-disable-next-line @lwc/lwc/no-inner-html
     container.innerHTML =
       `<pre data-start="${this.startingLineNumber}" data-line="${this.targetLineNumber}" data-line-offset="${this.targetLineNumber}">` +
-      `<code class="language-${this.language} line-numbers">${this.code}</code>` +
+      `<code class="language-${this.language}">${this.code}</code>` +
       `</pre>`;
-    // eslint-disable-next-line no-undef
-    this._Prism = Prism;
-    // Prism.hooks.add('complete', e => {
-    //   console.info('>>> complete', e);
-    // });
-    if (this._Prism) {
-      // For some reason, calling highlightAll() twice is necessary to get the line highlighting to work.
-      // When it's called only once, the line highlighting is not applied (when there is only one instance of the code viewer LWC on the page).
-      //
-      //
-      //
-      // Why? I don't know. (╯‵□′)╯︵┻━┻
-      //
-      //
-      //
-      // Is this a bug in Prism?
-      // Is this a bug in LWC?
-      // Is this a bug in Salesforce?
-      // Is this a bug in the universe?
-      //
-      //
-      //
-      // I don't know.
-      //
-      // I just simply don't know ¯\_(ツ)_/¯
-      //
-      //
-      //
-      // Perhaps there are some things in life that we'll never understand.
-      //
-      //
-      // Things that are simply beyond our comprehension.
-      //
-      //
-      // But I DO know is that (👉ﾟヮﾟ)👉 calling highlightAll() twice in a row 👈(ﾟヮﾟ👈) seems to circumvent the issue.
-      //
-      //
-      // So here we are, calling it twice...
-      //
-      // ...with just 1 extra line of code.....
-      // ........................................
-      // ......................
-      // ...............................
-      // ..............
-      // ........................................and a whole bunch of ridiculous comments about it ^_^
-      this._Prism.highlightAll(false, () => console.info('>>> made it here 1! '));
-      debugger;
-      // this._Prism.highlightAll(false, () => console.info('>>> made it here 2! '));
-      // o_O
-      // this._Prism.highlightAll();
-    }
-
+    this._highlightCode();
     this.isLoaded = true;
+  }
+
+  _highlightCode() {
+    // For some reason, calling highlightAll() twice is necessary to get the line highlighting to work.
+    // When it's called only once, the line highlighting is not applied (when there is only one instance of the code viewer LWC on the page).
+    //
+    //
+    //
+    // Why? I don't know. (╯‵□′)╯︵┻━┻
+    //
+    //
+    //
+    // Is this a bug in Prism?
+    // Is this a bug in LWC?
+    // Is this a bug in Salesforce?
+    // Is this a bug in the universe?
+    //
+    //
+    //
+    // I don't know.
+    //
+    // I just simply don't know ¯\_(ツ)_/¯
+    //
+    //
+    //
+    // Perhaps there are some things in life that we'll never understand.
+    //
+    //
+    // Things that are simply beyond our comprehension.
+    //
+    //
+    // But I DO know is that (👉ﾟヮﾟ)👉 calling highlightAll() twice in a row 👈(ﾟヮﾟ👈) seems to circumvent the issue.
+    //
+    //
+    // So here we are, calling it twice...
+    //
+    // ...with just 1 extra line of code.....
+    // ........................................
+    // ......................
+    // ...............................
+    // ..............
+    // ........................................and a whole bunch of ridiculous comments about it ^_^
+    //
+
+    // eslint-disable-next-line no-undef
+    this._Prism.highlightAll();
+    // o_O
+    // eslint-disable-next-line no-undef
+    this._Prism.highlightAll();
   }
 }
