d5a62011476a6da7c402b17d3dd028cad448401f
Jonathan Gillespie <jonathan.c.gillespie@gmail.com>
2025-07-28 23:39:59 -0400

    Added new LWC logEntryPageSection that uses PrismJS to display some fields in a better format than the standard fields/UI

---
+++ nebula-logger/core/main/log-management/lwc/logEntryPageSection/logEntryPageSection.js
@@ -0,0 +1,183 @@
+/*************************************************************************************************
+ * This file is part of the Nebula Logger project, released under the MIT License.               *
+ * See LICENSE file or go to https://github.com/jongpie/NebulaLogger for full license details.   *
+ ************************************************************************************************/
+
+import { LightningElement, api, wire } from 'lwc';
+import { getRecord, getFieldValue } from 'lightning/uiRecordApi';
+import getSchemaForName from '@salesforce/apex/LoggerSObjectMetadata.getSchemaForName';
+
+// LogEntry__c object and fields
+import LOG_ENTRY_OBJECT from '@salesforce/schema/LogEntry__c';
+import EXCEPTION_LOCATION_FIELD from '@salesforce/schema/LogEntry__c.ExceptionLocation__c';
+import EXCEPTION_MESSAGE_FIELD from '@salesforce/schema/LogEntry__c.ExceptionMessage__c';
+import EXCEPTION_STACK_TRACE_FIELD from '@salesforce/schema/LogEntry__c.ExceptionStackTrace__c';
+import EXCEPTION_TYPE_FIELD from '@salesforce/schema/LogEntry__c.ExceptionType__c';
+import HTTP_REQUEST_BODY_FIELD from '@salesforce/schema/LogEntry__c.HttpRequestBody__c';
+import HTTP_REQUEST_ENDPOINT_ADDRESS_FIELD from '@salesforce/schema/LogEntry__c.HttpRequestEndpointAddress__c';
+import HTTP_REQUEST_HEADERS_FIELD from '@salesforce/schema/LogEntry__c.HttpRequestHeaders__c';
+import HTTP_REQUEST_METHOD_FIELD from '@salesforce/schema/LogEntry__c.HttpRequestMethod__c';
+import HTTP_RESPONSE_BODY_FIELD from '@salesforce/schema/LogEntry__c.HttpResponseBody__c';
+import HTTP_RESPONSE_HEADERS_FIELD from '@salesforce/schema/LogEntry__c.HttpResponseHeaders__c';
+import HTTP_RESPONSE_STATUS_CODE_FIELD from '@salesforce/schema/LogEntry__c.HttpResponseStatusCode__c';
+import MESSAGE_FIELD from '@salesforce/schema/LogEntry__c.Message__c';
+import RELATED_RECORD_ID_FIELD from '@salesforce/schema/LogEntry__c.RecordId__c';
+import RELATED_RECORD_JSON_FIELD from '@salesforce/schema/LogEntry__c.RecordJson__c';
+import RELATED_RECORD_NAME_FIELD from '@salesforce/schema/LogEntry__c.RecordName__c';
+import STACK_TRACE_FIELD from '@salesforce/schema/LogEntry__c.StackTrace__c';
+
+// Define field sets for each section type
+const SECTION_FIELD_SETS = {
+  Message: [MESSAGE_FIELD, STACK_TRACE_FIELD],
+  Exception: [EXCEPTION_MESSAGE_FIELD, EXCEPTION_TYPE_FIELD, EXCEPTION_STACK_TRACE_FIELD, EXCEPTION_LOCATION_FIELD],
+  'HTTP Request': [HTTP_REQUEST_METHOD_FIELD, HTTP_REQUEST_ENDPOINT_ADDRESS_FIELD, HTTP_REQUEST_HEADERS_FIELD, HTTP_REQUEST_BODY_FIELD],
+  'HTTP Response': [HTTP_RESPONSE_STATUS_CODE_FIELD, HTTP_RESPONSE_HEADERS_FIELD, HTTP_RESPONSE_BODY_FIELD],
+  'Related Record Details': [RELATED_RECORD_ID_FIELD, RELATED_RECORD_JSON_FIELD, RELATED_RECORD_NAME_FIELD]
+};
+
+// Define which fields should use loggerCodeViewer and their language
+const CODE_VIEWER_FIELDS = {
+  ExceptionMessage__c: 'text',
+  ExceptionStackTrace__c: 'text',
+  HttpRequestBody__c: 'json',
+  HttpRequestHeaders__c: 'json',
+  HttpResponseBody__c: 'json',
+  HttpResponseHeaders__c: 'json',
+  Message__c: 'text',
+  StackTrace__c: 'text'
+};
+
+// Function to get the correct field name based on namespace
+function _getCodeViewerFieldName(fieldApiName) {
+  // Remove the __c suffix first, then check for namespace prefix
+  const withoutSuffix = fieldApiName.replace('__c', '');
+  const parts = withoutSuffix.split('__');
+
+  // If there are multiple parts, the last part is the base field name
+  // If there's only one part, it's already the base field name
+  const baseFieldName = parts.length > 1 ? parts[parts.length - 1] : parts[0];
+
+  // Add back the __c suffix
+  return baseFieldName + '__c';
+}
+
+// Function to check if a field should use code viewer
+function _shouldUseCodeViewer(fieldApiName) {
+  const baseFieldName = _getCodeViewerFieldName(fieldApiName);
+  return CODE_VIEWER_FIELDS.hasOwnProperty(baseFieldName);
+}
+
+// Function to get the language for a field
+function _getCodeViewerLanguage(fieldApiName) {
+  const baseFieldName = _getCodeViewerFieldName(fieldApiName);
+  return CODE_VIEWER_FIELDS[baseFieldName] || null;
+}
+
+export default class LogEntryPageSection extends LightningElement {
+  @api recordId;
+  @api sectionType;
+
+  objectApiName = LOG_ENTRY_OBJECT;
+  hasLoaded = false;
+  logEntry = {};
+  sectionFields = [];
+  sectionTitle = '';
+  sectionIcon = '';
+  fieldMetadata = {};
+
+  get sectionFieldSet() {
+    return SECTION_FIELD_SETS[this.sectionType] || [];
+  }
+
+  get sectionConfig() {
+    const configs = {
+      Message: {
+        title: 'Message',
+        icon: 'utility:message'
+      },
+      Exception: {
+        title: 'Exception Details',
+        icon: 'utility:error'
+      },
+      'HTTP Request': {
+        title: 'HTTP Request',
+        icon: 'utility:upload'
+      },
+      'HTTP Response': {
+        title: 'HTTP Response',
+        icon: 'utility:download'
+      },
+      'Related Record Details': {
+        title: 'Related Record Details',
+        icon: 'utility:record'
+      }
+    };
+    return configs[this.sectionType] || { title: this.sectionType, icon: 'utility:info' };
+  }
+
+  @wire(getRecord, {
+    recordId: '$recordId',
+    fields: '$sectionFieldSet'
+  })
+  async wiredGetLogEntry({ data, error }) {
+    if (data) {
+      this.logEntry = data;
+      this.sectionTitle = this.sectionConfig.title;
+      this.sectionIcon = this.sectionConfig.icon;
+
+      // Load field metadata for LogEntry__c
+      try {
+        this.fieldMetadata = await getSchemaForName({ sobjectApiName: 'LogEntry__c' });
+        this.sectionFields = this._buildSectionFields();
+      } catch (error) {
+        console.error('Error loading field metadata:', error);
+        this.sectionFields = this._buildSectionFields();
+      }
+
+      this.hasLoaded = true;
+    } else if (error) {
+      console.error('Error loading log entry:', error);
+      this.hasLoaded = true;
+    }
+  }
+
+  _buildSectionFields() {
+    const fields = [];
+
+    this.sectionFieldSet.forEach(field => {
+      const fieldValue = getFieldValue(this.logEntry, field);
+      if (fieldValue !== null && fieldValue !== undefined && fieldValue !== '') {
+        const fieldApiName = field.fieldApiName;
+        const shouldUseCodeViewer = _shouldUseCodeViewer(fieldApiName);
+        const codeViewerLanguage = shouldUseCodeViewer ? _getCodeViewerLanguage(fieldApiName) : null;
+
+        // Get field metadata from the schema
+        const fieldSchema = this.fieldMetadata.fields?.[fieldApiName];
+        const fieldLabel = fieldSchema?.label || fieldApiName;
+        const fieldType = fieldSchema?.type || 'string';
+        const isNumber = fieldType === 'int' || fieldType === 'double' || fieldType === 'number';
+
+        fields.push({
+          apiName: fieldApiName,
+          label: fieldLabel,
+          value: fieldValue,
+          type: fieldType,
+          isLongText: fieldType === 'string' && fieldValue && fieldValue.length > 255,
+          isNumber: isNumber,
+          useCodeViewer: shouldUseCodeViewer,
+          codeViewerLanguage: codeViewerLanguage
+        });
+      }
+    });
+
+    return fields;
+  }
+
+  get hasSectionData() {
+    return this.sectionFields.length > 0;
+  }
+
+  get showSection() {
+    return this.hasLoaded && this.hasSectionData;
+  }
+}
