109eed078d1a34138635a59934d34894c030b0f7
Jonathan Gillespie <jonathan.c.gillespie@gmail.com>
2025-11-18 14:18:19 -0500

    Upgraded PrismJS to 1.30 from 1.29 and fixed some code highlightning issues in loggerCodeViewer

--- nebula-logger/core/main/log-management/lwc/loggerCodeViewer/loggerCodeViewer.js
+++ nebula-logger/core/main/log-management/lwc/loggerCodeViewer/loggerCodeViewer.js
@@ -15,8 +15,6 @@ export default class LoggerCodeViewer extends LightningElement {
 
   isLoaded = false;
 
-  _Prism;
-
   async renderedCallback() {
     if (this.isLoaded) {
       return;
@@ -34,9 +32,56 @@ export default class LoggerCodeViewer extends LightningElement {
       `<pre data-start="${this.startingLineNumber}" data-line="${this.targetLineNumber}" data-line-offset="${this.targetLineNumber}">` +
       `<code class="language-${this.language}">${this.code}</code>` +
       `</pre>`;
-    // eslint-disable-next-line no-undef
-    this._Prism = Prism;
-    await this._Prism.highlightAll();
+    this._highlightCode();
     this.isLoaded = true;
+  }
+
+  _highlightCode() {
+    // For some reason, calling Prism.highlightAll() twice is necessary (see below) to get the line highlighting to work.
+    // When it's called only once, the line highlighting is not applied (when there is only one instance of the code viewer LWC on the page).
+    //
+    //
+    //
+    // Why? I don't know. (╯‵□′)╯︵┻━┻
+    //
+    //
+    //
+    // Is this a bug in Prism?
+    // Is this a bug in LWC?
+    // Is this a bug in Salesforce?
+    // Is this a bug in the universe?
+    //
+    //
+    //
+    // I don't know.
+    //
+    // I just simply don't know ¯\_(ツ)_/¯
+    //
+    //
+    //
+    // Perhaps there are some things in life that we'll never understand.
+    //
+    //
+    // Things that are simply beyond our comprehension.
+    //
+    //
+    // But I DO know is that (👉ﾟヮﾟ)👉 calling highlightAll() twice in a row 👈(ﾟヮﾟ👈) seems to circumvent the issue.
+    //
+    //
+    // So here we are, calling it twice...
+    //
+    // ...with just 1 extra line of code.....
+    // ........................................
+    // ......................
+    // ...............................
+    // ..............
+    // ........................................and a whole bunch of ridiculous comments about it ^_^
+    //
+
+    // eslint-disable-next-line no-undef
+    Prism.highlightAll();
+    // o_O
+    // eslint-disable-next-line no-undef
+    Prism.highlightAll();
   }
 }
